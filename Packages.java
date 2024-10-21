b33f9308a25f951fc27a8938fab49e3c76a1de37
Owino Michael <57246712+OwinoMichael@users.noreply.github.com>
2024-10-21 19:33:24 -0400

    Create Packages.java
    Packages

---
+++ Packages.java
@@ -0,0 +1,16 @@
+/**             PACKAGES
+ *      These are just the file and folder structures in Java
+ *      There are some inbuilt classes & interfaces in Java which are stored in unique packages (files and folders)
+ *      In order to access the Classes e.g. ArrayList, we import the package
+ *      " package java.util.ArrayList " we are accessing inbuilt class called ArrayList in Java/Utils/ArrayList.java
+ *
+ *      When building projects/Libraries, we also create our own packages
+ *      E.g. A package for Google would be "com.google.maps" to access their map library
+ *      Reversing the domain name makes it UNIQUE
+ *      We should also make our own libraries unique com.owino.encodeJava
+ *
+ * */
+
+
+public class Packages {
+}
